a3237ae9d9886eb6c0d6da9299ca417dba042962
Cedric DUE <cedric@saagie.com>
2018-03-27 13:32:53 +0200

    feat: configure Spark

--- run-dataiku.sh
+++ run-dataiku.sh
@@ -6,20 +6,35 @@ DSS_INSTALLDIR="/home/dataiku/dataiku-dss-$DSS_VERSION"
 echo "Running DSS"
 /home/dataiku/run.sh &
 
-# wait a few sec to be sure everything runs fine
+# Wait a few sec to be sure everything runs fine
 sleep 30
 
 # Copy JDBC jars and dependencies
 mkdir -p /home/dataiku/dss/lib/jdbc
 cp /home/dataiku/lib/* /home/dataiku/dss/lib/jdbc
 
-# stop DSS
+# Stop DSS
 "$DSS_DATADIR"/bin/dss stop
 
 # setting up DSS Hadoop integration
 "$DSS_DATADIR"/bin/dssadmin install-hadoop-integration
 
-# setting up spark
+# Setting up spark
+# As volumes are mounted at container startup,
+# we need to grab mounted Spark conf and overwrite the default one before
+# before running Zepplin
+if [ -f "/usr/local/spark/conf/spark-env.sh" ]
+then
+  # overwrite Spark 2.1.0 config
+  echo "INFO: ovewriting default spark-env.sh"
+  cp /usr/local/spark/conf/spark-env.sh /opt/spark/conf
+  chmod 755 /opt/spark/conf/spark-env.sh
+else
+  # use default config
+  echo "WARNING: NO CUSTOM spark-env.sh PROVIDED. USING DEFAULT TEMPLATE."
+  cp /opt/spark/conf/spark-env.sh.template /opt/spark/conf/spark-env.sh
+fi
+
 "$DSS_DATADIR"/bin/dssadmin install-spark-integration -sparkHome /opt/spark
 
 # wait a few sec
